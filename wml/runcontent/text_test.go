07c15cbc4ac7a4dee5edc73754cae4cd20e6d26b
gomutex <gopointer@proton.me>
2024-06-24 10:04:04 +0530

    test update

--- wml/runcontent/text_test.go
+++ wml/runcontent/text_test.go
@@ -2,9 +2,10 @@ package runcontent
 
 import (
 	"encoding/xml"
-	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/gomutex/godocx/internal"
 )
 
 func TestTextMarshalXML(t *testing.T) {
@@ -13,7 +14,7 @@ func TestTextMarshalXML(t *testing.T) {
 		expected string
 	}{
 		{NewText(), `<w:t></w:t>`},
-		{TextFromString("Hello, World!"), `<w:t xml:space="preserve">Hello, World!</w:t>`},
+		{TextFromString("Hello, World!"), `<w:t>Hello, World!</w:t>`},
 	}
 
 	for _, tc := range testCases {
@@ -44,26 +45,38 @@ func TestTextUnmarshalXML(t *testing.T) {
 		expected *Text
 	}{
 		{`<w:t></w:t>`, NewText()},
-		{`<w:t xml:space="preserve">Hello, World!</w:t>`, TextFromString("Hello, World!")},
-		{`<w:t xml:space="preserve">Some text</w:t><w:other>Other element</w:other>`, TextFromString("Some text")},
+		{`<w:t xml:space="preserve">Hello, World!</w:t>`, &Text{
+			text:  "Hello, World!",
+			space: internal.ToPtr("preserve"),
+		}},
+		{`<w:t xml:space="preserve">Some text</w:t>`, &Text{
+			text:  "Some text",
+			space: internal.ToPtr("preserve"),
+		}},
 	}
 
 	for _, tc := range testCases {
-		decoder := xml.NewDecoder(strings.NewReader(tc.input))
-		var result Text
+		t.Run(tc.input, func(t *testing.T) {
+			decoder := xml.NewDecoder(strings.NewReader(tc.input))
+			var result Text
 
-		startToken, err := decoder.Token()
-		if err != nil {
-			t.Errorf("Error getting start token: %v", err)
-		}
+			startToken, err := decoder.Token()
+			if err != nil {
+				t.Fatalf("Error getting start token: %v", err)
+			}
 
-		err = result.UnmarshalXML(decoder, startToken.(xml.StartElement))
-		if err != nil {
-			t.Errorf("Error during UnmarshalXML: %v", err)
-		}
+			err = result.UnmarshalXML(decoder, startToken.(xml.StartElement))
+			if err != nil {
+				t.Fatalf("Error during UnmarshalXML: %v", err)
+			}
 
-		if !reflect.DeepEqual(&result, tc.expected) {
-			t.Errorf("Expected %+v, but got %+v", tc.expected, &result)
-		}
+			if result.text != tc.expected.text {
+				t.Errorf("Expected text %q, but got %q", tc.expected.text, result.text)
+			}
+
+			if err := internal.ComparePtr("space", tc.expected.space, result.space); err != nil {
+				t.Errorf(err.Error())
+			}
+		})
 	}
 }
